1a3f2b306e1364baa5a15ecb2f657e1bf09a806b
yoshikouki <yoshikouki@gmail.com>
2024-02-21 22:56:09 +0900

    Build daisyUI

--- app/routes/_renderer.tsx
+++ app/routes/_renderer.tsx
@@ -4,7 +4,7 @@ import { Script } from "honox/server";
 
 export default jsxRenderer(({ children, title }) => {
   return (
-    <html lang="en">
+    <html lang="en" data-theme="black">
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
